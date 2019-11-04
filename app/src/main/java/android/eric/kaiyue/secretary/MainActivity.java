493f8efa4009508d5bf9533cd338b9be751af988
kaiyue <eric.kaiyue@gmail.com>
2019-11-03 18:55:45 -0800

    first commit

---
+++ app/src/main/java/android/eric/kaiyue/secretary/MainActivity.java
@@ -0,0 +1,15 @@
+package android.eric.kaiyue.secretary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private imageview reminderView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
